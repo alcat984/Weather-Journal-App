f10ef55c832c08ed729f3aabda6943f7c3ed8a12
Silvio Alejandro Catalano <silviocat84@gmail.com>
2020-12-13 10:36:01 +0100

    Fourth commit

--- server.js
+++ server.js
@@ -1,39 +1,41 @@
-/** Define the object variable as an empty 
+/*  Define the object variable as an empty 
   array to store incoming data */
 
   let projectData = {};
 
-  /**Require the express to run the server*/ 
+  /* Express to run server and routes */ 
+  
   const express = require('express');
   
-  // const dotenv = require('dotenv');   
-  // dotenv.config(); 
+    
+  // Start up an instance of app /
   
-  // const API_KEY = process.env.API_KEY;
-  
-  
-  
-  /**Start up an instance of app */
   const app = express();
   
-  /**Dependencies */
+  /* Dependencies */
+
   const bodyParser = require('body-parser'); 
   
-  /**Middleware */
+  /* Middleware */
+
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   
+  /* Cors for cross origin allowance */
+
   const cors = require('cors');
   app.use(cors());
   
-  /**Initialize the main project folder */
+  /* Initialize the main project folder */
+  
   app.use(express.static('website'));
   
-  /**Assign a port to the server */
-  const port = process.env.PORT || 3000;
+  /* Assign a port to the server */
+
+  const port = 3000;
+    
+  /* Spin up the server */
   
-  
-  /* Spin up the server*/
   const server = app.listen(port, listening);
    function listening(){
       // console.log(server);
@@ -41,24 +43,3 @@
       console.log(`Server running on localhost: ${port}`);
     };
   
-  /**Get route */
-  
-  app.get('/all', function(req, res) {
-            res.send(projectData);
-  })    
-    
-  
-  /**TODO route */
-  
-  app.post('/addWeather', function(req, res) {
-            console.log(req.body)
-            newEntry = {
-              name: req.body.name,
-              icon: req.body.icon,
-              temp_min: req.body.temp_min,
-              description: req.body.description
-            }
-            projectData = newEntry;
-            res.send(projectData);
-            console.log({message: "Post Recieved"})
-  });
--- website/app.js
+++ website/app.js
@@ -5,3 +5,83 @@ const baseURL = '';
 const countryCode = '';
 const unitOfMeasurement = 'units=metric';
 const API_KEY = `&appid=b84c4718d193be816170a47fab094d88`;
+
+const currentWeather = document.getElementById('zip');
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+    e.preventDefault();
+    let url;
+
+    if(zip) {
+        url = baseURL + currentWeather.value + countryCode + unitOfMeasurement + API_KEY;
+        }else{
+            console.log({ message: 'Bad url call!'})
+        }
+        getWeatherData(url)
+        .then(function (projectData){
+            return projectData;
+        })
+}
+
+//Here we can add the Async function to GET request from OpenWeatherApi
+
+const getWeatherData = async (url) => {
+
+    const response = await fetch(url)
+    try {
+              const data = await response.json();
+              console.log(data)
+              //Add data to post request
+              postData ('/addWeather', {name: data['name'], icon: data['weather'][0]['icon'], temp_min: data['main']['temp_min'].toFixed(), 
+                            description: data['weather'][0]['description']
+                  }).then (updateUI())
+                  return data;
+                  } catch(error) {
+                              console.log({message: "Cannot fetch url from server correctly!"});
+                  }
+}
+
+const postData = async (url="", data = {}) => {
+    console.log(data);
+    const response = await fetch(url, {
+         method: 'POST',
+         credentials: 'same-origin',
+         headers: {
+                   'Content-Type': 'application/json',
+                  //  'Accept': 'application/json'
+         },
+         body: JSON.stringify(data) //Body data-type matches the 'Content-Type' header   
+    })
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    } catch (error) {
+              console.log({message: 'Bad response recieved!'});
+              
+    }
+};
+
+
+const updateUI = async () => {
+    const response = await fetch('/all');
+    try {
+              const allData = await response.json();
+              const name = document.querySelector('.name');
+              const icon = document.querySelector('.icon');
+              const temp = document.querySelector('.temp');
+              const description = document.querySelector('.description');
+              const date = document.querySelector('.date');
+              
+              name.textContent = allData.name;
+              icon.textContent = allData.icon;
+              temp.textContent = allData.temp_min;
+              description.textContent = allData.description;
+              date.textContent = allData.date = new Date().toDateString();
+             
+    } catch (error) {
+              console.log({message: 'Invalid zipcode input!'}); 
+    }
+};
