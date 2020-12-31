895be207655b9dc4b75c5e03f2eb5455914111df
Silvio Alejandro Catalano <silviocat84@gmail.com>
2020-12-31 10:19:49 +0100

    added api URL

--- website/app.js
+++ website/app.js
@@ -1,8 +1,8 @@
 /* Global Variables for OpenWeather API request*/
 
 // Create a new date instance dynamically with JS
-const baseURL = '';
-const countryCode = '';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const countryCode = ',de&';
 const unitOfMeasurement = 'units=metric';
 const API_KEY = `&appid=b84c4718d193be816170a47fab094d88`;
 
