60d432d89f2bd62bd2f0cff3316cfea28b034403
Silvio Alejandro Catalano <silviocat84@gmail.com>
2021-01-01 10:55:48 +0100

    updated country code to ita

--- website/app.js
+++ website/app.js
@@ -2,7 +2,7 @@
 
 // Create a new date instance dynamically with JS
 const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
-const countryCode = ',it&';
+const countryCode = ',ita&';
 const unitOfMeasurement = 'units=metric';
 const API_KEY = `&appid=79d40003135e0c3584fe885d1ec08b98`;
 
